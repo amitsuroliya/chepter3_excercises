35be89151132f8eda1f615de2f9f4d87e74e03e6
Amit Kumar Suroliya <amitkumarsuroliya@gmail.com>
2012-09-03 14:08:12 +0530

    Update spec/requests/static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,7 +36,7 @@ describe "Static pages" do
     end
   end
 
-  describe "About page" do
+  describe "Contact page" do
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact')
